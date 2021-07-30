5b7a35146705b2c0557ebd75f6a9b5b4f3c215e9
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-07-30 01:00:58 -0400

    Added a GET route to get all posts for homepage

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { News, User } = require("./models/indexModels");
+const { News, User } = require("./models");
 
 const express = require('express');
 const exphbs  = require('express-handlebars');
@@ -12,6 +12,8 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(require('./controllers/'));
 
 app.get('/', function (req, res) {
     res.render('homepage');
